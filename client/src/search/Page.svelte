4b91cbe0766cbd4bd9d5b6008c824bbd26c5c8a8
Peter Saxton <peterhsaxton@gmail.com>
2020-09-16 11:38:36 +0100

    add times on search results

--- client/src/search/Page.svelte
+++ client/src/search/Page.svelte
@@ -49,7 +49,10 @@
   <nav id="results">
     {#each results as {id, next, topic, participants}}
     <a class="block my-2 py-1 px-2 rounded border border-l-4 text-gray-800 bg-white focus:outline-none focus:text-gray-900 focus:border-indigo-800 hover:border-indigo-800 focus:shadow-xl" href="/c/{id}#{next}">
-      <h2 class="font-bold my-1">{topic}</h2>
+      <div class="flex">
+        <h2 class="flex-grow font-bold my-1">{topic}</h2>
+        <span class="my-1">{updated_at}</span>
+      </div>
       <div class="truncate">
         {participants}
       </div>
