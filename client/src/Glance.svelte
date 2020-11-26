566d983d022b1ee0eae88171331c9531ddbc08a8
Peter Saxton <peterhsaxton@gmail.com>
2020-11-26 19:55:40 +0000

    add the glance component

---
+++ client/src/Glance.svelte
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+  export let href;
+  export let text;
+  let url = new URL(href, window.location.origin);
+</script>
+
+{#if url.host === "whereby.com"}
+  <a class="flex items-center items-stretch" {href} target="_blank">
+    <img class="w-10" src="https://d32wid4gq0d4kh.cloudfront.net/favicon_whereby-196x196.png" alt="">
+    <span class="align-middle p-2 border-b-4" style="border-color:#f8dbd5;">{text.trim() || 'Meet with Whereby'}</span>
+  </a>
+{:else}
+<a { href }>
+  { text }
+</a>
+{/if}
