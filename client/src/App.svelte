daee5695410bb21d82d21136d6917bdc082964ae
Peter Saxton <peterhsaxton@gmail.com>
2020-12-12 06:36:59 +0000

    start router

---
+++ client/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Home from "./pages/Home.svelte";
+  // import Begin from "./pages/Begin.svelte"
+  // import Archive from "./pages/Archive.svelte"
+
+  import router from "page";
+
+  let page
+  router('/', () => page = Home)
+
+  router.start()
+</script>
+
+<svelte:component this={page} />
