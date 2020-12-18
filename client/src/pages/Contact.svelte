4ba956bca643e008524afd9d79803a2ed58ab353
Peter Saxton <peterhsaxton@gmail.com>
2020-12-18 12:00:56 +0000

    handle clearing annotations

--- client/src/pages/Contact.svelte
+++ client/src/pages/Contact.svelte
@@ -18,12 +18,15 @@
   let contact;
   let previous
   fetchContact(emailAddressFor(identifier)).then(function ({data}) {
-    contact = data
-    if (contact.introduction) {
-      previous = [{blocks: parse(contact.introduction), author: contact.emailAddress}];
-    } else {
-      previous = [];
-    }
+    // TODO remove
+    setTimeout(function () {
+      contact = data
+      if (contact.introduction) {
+        previous = [{blocks: parse(contact.introduction), author: contact.emailAddress}];
+      } else {
+        previous = [];
+      }
+    }, 1000);
   })
 
   let draft = "";
@@ -114,7 +117,6 @@
   <!-- extract rounding article a a thing -->
   {#if preview}
   <!-- TODO make sure can't always add annotation, or make it work with self -->
-  {JSON.stringify(current)}
   <Note blocks={current.blocks} notes={previous} index={previous.length} author={"me"}>
     <div class="mt-2 pl-12 flex items-center">
       <div class="flex flex-1">
@@ -144,7 +146,7 @@
   <article class="my-4 py-6 pr-12 bg-white rounded-lg shadow-md ">
     <!-- Could do an on submit and catch whats inside -->
     <!-- TODO name previous inside composer -->
-    <Composer bind:annotations notes={previous} bind:draft/>
+    <Composer bind:annotations notes={previous} bind:draft on:clearAnnotation={clearAnnotation}/>
     <div class="mt-2 pl-12 flex items-center">
       <div class="flex flex-1">
         <!-- TODO this needs to show your email address, or if in header nothing at all -->
@@ -162,6 +164,32 @@
   </article>
   {/if}
   {:else}
-  loading
+  <article class="my-4 py-6 pr-12 bg-white rounded-lg shadow-md ">
+    <div class="w-2/3 bg-gray-200 ml-12 h-4 rounded mb-4">
+
+    </div>
+    <div class="w-1/3 bg-gray-200 ml-12 h-4 rounded mb-4">
+
+    </div>
+    <div class="w-1/2 bg-gray-200 ml-12 h-4 rounded mb-20">
+
+    </div>
+    <!-- Could do an on submit and catch whats inside -->
+    <!-- TODO name previous inside composer -->
+    <div class="mt-2 pl-12 flex items-center">
+      <div class="flex flex-1">
+        <!-- TODO this needs to show your email address, or if in header nothing at all -->
+        <!-- <span class="font-bold text-gray-700 mr-1">From:</span>
+        <input class="flex-grow mr-2 bg-white border-white flex-grow focus:border-gray-700 outline-none placeholder-gray-700" bind:value={contact} type="email" placeholder="Your email address" required> -->
+      </div>
+      <button class="flex-grow-0 py-2 px-6 rounded-lg bg-gray-500  text-gray-200 font-bold" type="submit" on:click={() => {preview = true}}>
+        <!-- <svg class="fill-current inline w-4 mr-2" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24">
+          <path d="m8.75 17.612v4.638c0 .324.208.611.516.713.077.025.156.037.234.037.234 0 .46-.11.604-.306l2.713-3.692z"></path>
+          <path d="m23.685.139c-.23-.163-.532-.185-.782-.054l-22.5 11.75c-.266.139-.423.423-.401.722.023.3.222.556.505.653l6.255 2.138 13.321-11.39-10.308 12.419 10.483 3.583c.078.026.16.04.242.04.136 0 .271-.037.39-.109.19-.116.319-.311.352-.53l2.75-18.5c.041-.28-.077-.558-.307-.722z"></path>
+        </svg> -->
+        Loading
+      </button>
+    </div>
+  </article>
   {/if}
 </main>
