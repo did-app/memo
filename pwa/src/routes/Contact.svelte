b14b3fde9699a1ba0e76011f6309ceb0dfa702c2
Peter Saxton <peterhsaxton@gmail.com>
2021-01-11 11:30:03 +0000

    create composer component

--- pwa/src/routes/Contact.svelte
+++ pwa/src/routes/Contact.svelte
@@ -1,7 +1,6 @@
 <script lang="typescript">
   import router from "page";
   import { onMount } from "svelte";
-  import { autoResize } from "../svelte/textarea";
   import type { Reference, Memo } from "../conversation";
   import * as Conversation from "../conversation";
   import type { Contact, Stranger } from "../social";
@@ -11,7 +10,8 @@
   import type { Block, Annotation, Prompt } from "../writing";
   import * as Writing from "../writing";
 
-  import Fragment from "../components/Fragment.svelte";
+  import type Composer__SvelteComponent_ from "../components/Composer.svelte";
+  import Composer from "../components/Composer.svelte";
   import MemoComponent from "../components/Memo.svelte";
   import BlockComponent from "../components/Block.svelte";
   import LinkComponent from "../components/Link.svelte";
@@ -24,8 +24,9 @@
   }
   let state: Authenticated = stateAll as Authenticated;
 
+  let composer: Composer__SvelteComponent_;
+
   export let emailAddress: string;
-  // state = state as Authenticated;
 
   // TODO this needs to be a promise of old ones we load up
   let contact = Social.contactForEmailAddress(state.contacts, emailAddress);
@@ -58,10 +59,6 @@
   // let reply: boolean = threadId === undefined;
   let reply: boolean = false;
 
-  let draft = "";
-  let blocks: Block[];
-  let preview = false;
-
   let root: HTMLElement;
 
   let userFocus: Reference | null = null;
@@ -93,8 +90,8 @@
           memos,
           state.me.emailAddress
         );
-        annotations = references.map(function (reference) {
-          return { reference, raw: "" };
+        references.map(function (reference) {
+          composer.addAnnotation(reference);
         });
       }
       return response;
@@ -103,77 +100,20 @@
     }
   })();
 
-  function mapAnnotation(draft: {
-    reference: Reference;
-    raw: string;
-  }): Annotation[] {
-    const { reference, raw } = draft;
-    let blocks = Writing.parse(raw);
-    if (blocks) {
-      return [
-        {
-          type: "annotation",
-          reference,
-          blocks,
-        },
-      ];
-    } else {
-      return [];
-    }
-  }
-
-  type AnnotationSpace = {
-    reference: Reference;
-    raw: string;
-  };
-  let annotations: AnnotationSpace[] = [];
-
   // DOESNT WORK ON ACTIVE message
   function quoteInReply() {
     if (focusSnapshot === null) {
       console.warn("Shouldn't be quoting without focus");
     } else {
-      let annotation = { reference: focusSnapshot, raw: "" };
-      annotations = [...annotations, annotation];
+      composer.addAnnotation(focusSnapshot);
     }
     reply = true;
   }
 
-  function clearAnnotation(index: number) {
-    annotations.splice(index, 1);
-    annotations = annotations;
-  }
-
-  // I still think at the end we should pull out very flat variables. but helpers work on known data types
-
-  $: blocks = (function (): Block[] {
-    let content = Writing.parse(draft);
-    let mappedAnnotations: Annotation[] = annotations.flatMap(mapAnnotation);
-    return content ? [...mappedAnnotations, ...content] : mappedAnnotations;
-  })();
-  let current: Memo;
-  $: current = {
-    content: blocks,
-    author: state.me.emailAddress,
-    posted_at: new Date(),
-    position: Conversation.currentPosition(contact.thread) + 1,
-  };
-  let suggestedPrompts: Prompt[] = [];
-  $: suggestedPrompts = preview
-    ? suggestedPrompts
-    : Conversation.makeSuggestions(
-        blocks,
-        Conversation.currentPosition(contact.thread) + 1
-      );
-  function clearPrompt(index: number) {
-    suggestedPrompts.splice(index, 1);
-    suggestedPrompts = suggestedPrompts;
-  }
-
-  function postMemo() {
+  function postMemo(content: Block[]) {
     Sync.postMemo(
       contact,
-      [...blocks, ...suggestedPrompts],
+      content,
       Conversation.currentPosition(contact.thread) + 1
     );
     router.redirect("/");
@@ -188,15 +128,6 @@
       console.warn("can't acknowledge stranger");
     }
   }
-  function referenceAuthor(peers: Memo[], reference: Reference) {
-    let memo = peers[reference.memoPosition - 1];
-    if (memo) {
-      return memo.author;
-    } else {
-      throw "Should have crashed on referemce";
-    }
-    // Probably follow reference should return author
-  }
 </script>
 
 <style>
@@ -208,12 +139,6 @@
     .sidebar {
       grid-template-columns: minmax(0px, 1fr) 20rem;
     }
-  }
-  textarea.message {
-    min-height: 8rem;
-  }
-  textarea.comment {
-    max-height: 25vh;
   }
 </style>
 
@@ -249,43 +174,17 @@
             class="my-4 py-6  pr-6 md:pr-12 bg-white rounded-lg shadow-md sticky bottom-0 border overflow-y-auto"
             style="max-height: 60vh;">
             {#if reply}
-              {#if preview}
-                <header class="ml-6 md:ml-12 mb-6 flex text-gray-600">
-                  <span class="font-bold">{state.me.emailAddress}</span>
-                  <span class="ml-auto">{new Date().toLocaleDateString()}</span>
-                </header>
-                <Fragment {blocks} peers={response.data} />
-                {#if suggestedPrompts.length !== 0}
-                  <h3 class="ml-6 md:ml-12 font-bold mt-4">
-                    Ask the following as highlighted questions.
-                  </h3>
-                {/if}
-                {#each suggestedPrompts as prompt, index}
-                  <div class="flex my-1">
-                    <div
-                      class="w-8 m-1 cursor-pointe preview ? suggestedPrompts :r flex-none"
-                      on:click={() => clearPrompt(index)}>
-                      <div class="w-6">
-                        <Icons.Bin />
-                      </div>
-                    </div>
-                    <div>
-                      {#each Conversation.followReference(prompt.reference, [
-                        ...response.data,
-                        current,
-                      ]) as block, index}
-                        <BlockComponent {block} {index} peers={response.data} />
-                      {/each}
-                    </div>
-                  </div>
-                {/each}
-
+              <Composer
+                let:content
+                let:back
+                bind:this={composer}
+                emailAddress={state.me.emailAddress}
+                peers={response.data}
+                position={response.data.length + 1}>
                 <div class="mt-2 pl-6 md:pl-12 flex items-center">
                   <div class="flex flex-1" />
                   <button
-                    on:click={() => {
-                      preview = false;
-                    }}
+                    on:click={back}
                     class="flex items-center rounded px-2 inline-block ml-auto border-gray-500 border-2">
                     <span class="w-5 mr-2 inline-block">
                       <Icons.ReplyAll />
@@ -294,7 +193,7 @@
                   </button>
 
                   <button
-                    on:click={postMemo}
+                    on:click={() => postMemo(content)}
                     class="flex items-center bg-gray-800 border-2 border-gray-800 text-white rounded px-2 ml-2">
                     <span class="w-5 mr-2 inline-block">
                       <Icons.Send />
@@ -302,71 +201,7 @@
                     <span class="py-1"> Send </span>
                   </button>
                 </div>
-              {:else}
-                <!-- TODO name previous inside composer -->
-                {#each annotations as { reference, raw }, index}
-                  <div class="flex my-1">
-                    <div
-                      class="w-8 m-2 cursor-pointer flex-none"
-                      on:click={() => clearAnnotation(index)}>
-                      <div class="w-4">
-                        <Icons.Bin />
-                      </div>
-                    </div>
-                    <div class="w-full border-purple-500 border-l-4">
-                      <blockquote class=" px-2">
-                        <div class="opacity-50">
-                          {#each Conversation.followReference(reference, response.data) as block, index}
-                            <BlockComponent
-                              {block}
-                              {index}
-                              peers={response.data} />
-                          {/each}
-                        </div>
-                        <a
-                          class="text-purple-800"
-                          href="#{reference.memoPosition}"><small>{referenceAuthor(response.data, reference)}</small></a>
-                      </blockquote>
-                      <div class="px-2">
-                        <textarea
-                          class="comment w-full bg-white outline-none"
-                          bind:value={raw}
-                          use:autoResize
-                          rows="1"
-                          autofocus
-                          placeholder="Your comment ..." />
-                      </div>
-                    </div>
-                  </div>
-                {/each}
-                <textarea
-                  class="message w-full bg-white outline-none pl-6 md:pl-12"
-                  use:autoResize
-                  bind:value={draft}
-                  placeholder="Your message ..." />
-                <div class="mt-2 pl-6 md:pl-12 flex items-center">
-                  <div class="flex flex-1 min-w-0">
-                    <span class="font-bold text-gray-700 mr-1">From:</span>
-                    <input
-                      class="flex-grow mr-2 bg-white border-white flex-grow focus:border-gray-700 outline-none placeholder-gray-700"
-                      bind:value={state.me.emailAddress}
-                      type="email"
-                      placeholder="Your email address"
-                      readonly
-                      required />
-                  </div>
-                  <button
-                    on:click={() => {
-                      preview = true;
-                    }}
-                    class="flex items-center bg-gray-800 border-2 border-gray-800 text-white rounded px-2 ml-2">
-                    <span class="w-5 mr-2 inline-block">
-                      <Icons.Send />
-                    </span>
-                    <span class="py-1"> Preview </span>
-                  </button>
-                </div>
-              {/if}
+              </Composer>
             {:else}
               {#if userFocus}
                 <div
@@ -376,7 +211,7 @@
                   {/each} -->
                   {#each Conversation.followReference(userFocus, [
                     ...response.data,
-                    current,
+                    // current,
                   ]) as block, index}
                     <BlockComponent {block} {index} peers={response.data} />
                   {/each}
@@ -421,7 +256,7 @@
                     </span>
                     <span class="py-1">
                       <!-- TODO Icons.scribe  -->
-                      {#if draft.trim().length !== 0}Draft{:else}Reply{/if}
+                      {#if false}Draft{:else}Reply{/if}
                     </span>
                   </button>
                 {/if}
