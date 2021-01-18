5023eca4623cf9f66a3c36e57f8a49857523b846
Richard Shepherd <richard@richardesigns.co.uk>
2021-01-18 16:38:49 +0000

    home tidy

--- pwa/src/routes/Home.svelte
+++ pwa/src/routes/Home.svelte
@@ -32,8 +32,10 @@
         <article
           class="bg-gray-800 border-l-8 border-r-8 border-green-500 md:px-12 my-4 p-4 rounded shadow-md text-white"
         >
-          <h2 class="font-bold">Available to install</h2>
-         
+          <h2 class="font-bold">Web-app download available</h2>
+            <p>
+              Install Memo's web-app on your computer, tablet or smartphone for faster access.
+            </p>          
           <button
             on:click={f.prompt}
             class="bg-green-500 flex hover:bg-green-600 items-center mt-4 px-4 rounded text-white">
