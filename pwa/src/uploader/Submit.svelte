0dbdc779e2ca5978cadb229e0c2c16424899ef99
Peter Saxton <peterhsaxton@gmail.com>
2021-03-05 13:57:56 +0000

    show errors in uploader

--- pwa/src/uploader/Submit.svelte
+++ pwa/src/uploader/Submit.svelte
@@ -4,12 +4,16 @@
   type Uploader = { name: string; parent_id: string; parent_name: string };
   let uploader: Uploader | null = null;
   let sending = false;
+  let lastSent: string | null = null;
+  let error: string | null = null;
 
   async function sendFile(
     event: Event & { currentTarget: EventTarget & HTMLFormElement }
   ) {
     event.preventDefault();
     sending = true;
+    lastSent = null;
+    error = null;
 
     try {
       let form: HTMLFormElement = event.currentTarget;
@@ -18,7 +22,6 @@
       if (!file) {
         throw "Needs a file";
       }
-      console.log(file);
 
       let url = apiOrigin + "/drive_uploaders/" + id + "/start";
       let response = await fetch(url, {
@@ -26,26 +29,24 @@
         body: JSON.stringify({ name: file.name, mime_type: file.type }),
       });
       let { location } = await response.json();
-      console.log(location);
-      let uploading = await fetch(location, {
-        // credentials: "include",
+      await fetch(location, {
         method: "PUT",
         headers: {
           "content-range": `bytes 0-${file.size - 1}/${file.size}`,
         },
         body: file,
       });
-      console.log(uploading);
+      lastSent = file.name;
+    } catch {
+      error = "Failed to send file";
     } finally {
       sending = false;
     }
   }
-  (async function (params) {
+  (async function () {
+    // Don't need to handle error case here because we use id from hash for upload
     let response = await fetch(apiOrigin + "/drive_uploaders/" + id);
-    console.log(response);
     let data = await response.json();
-    console.log(data);
-
     uploader = data.uploader;
   })();
 </script>
@@ -53,7 +54,6 @@
 <div class="p-4 border-4 border-dashed">
   {#if uploader}
     {uploader.name}
-    <!-- {JSON.stringify(uploader)} -->
   {:else}
     Loading
   {/if}
@@ -61,7 +61,12 @@
   {#if sending}
     Working
   {:else}
-    <!-- Query for embed or /pub vs /embed -->
+    {#if error}
+      {error}
+    {/if}
+    {#if lastSent}
+      lastSent
+    {/if}
     <form on:submit={sendFile}>
       <input type="file" />
       <button>Submit</button>
